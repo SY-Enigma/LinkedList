16cacb8bd83633f5622dd167e8104c61bdfe1e1b
17974 <1797441048@qq.com>
2020-10-05 21:49:37 +0800

    转移的

--- src/com/sy/week1/Fac.java
+++ src/com/sy/week1/Fac.java
@@ -6,6 +6,16 @@ package com.sy.week1;
  * @Author SY
  * @Date 2020/10/5
  **/
+
 public class Fac {
+    public static void main(String[] args) {
+        int n = 10;
+        int result = 1;
+        for (int i = 1; i <= n ; i++) {
+
+            result=result *i;
+        }
+        System.out.println(n +"的阶乘是" + result);
+    }
 
 }
